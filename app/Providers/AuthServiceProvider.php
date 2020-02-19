2545dd08845c03d781a7479160a2f4deac321c0d
Andrey Artyushenko <work.a.andrey@gmail.com>
2020-02-19 23:23:24 +0500

    add auth for horizon

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,6 +26,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('viewHorizon', function ($user) {
+            return true;
+        });
     }
 }
